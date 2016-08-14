934c5ee48b1c712d16cc0d5373b3c3ed76cc25c0
Dan Wiebe <dwiebe@pillartechnology.com>
2016-08-14 15:46:30 -0400

    Converters are hardened a bit

--- src/converters.c
+++ src/converters.c
@@ -7,7 +7,7 @@ typedef struct {
   int value;
 } ValueElement;
 
-ValueElement valueElements[] = {
+const ValueElement valueElements[] = {
   {"M", 1000},
   {"CM", 900},
   {"D", 500},
@@ -48,6 +48,8 @@ const ValueElement* findElementWithPrefix (const char* p) {
 }
 
 int RC_INVALID_ROMAN_NUMERAL = 1;
+int RC_NUMBER_OUT_OF_RANGE = 2;
+int RC_OUTPUT_OVERRUN = 3;
 
 /********************/
 /* PUBLIC INTERFACE */
@@ -56,17 +58,39 @@ int RC_INVALID_ROMAN_NUMERAL = 1;
 int convertRtoA (const char* roman, int* arabic) {
   int valueSoFar = 0;
   const char* p = roman;
+  const ValueElement* valueElement;
 
   while (*p != 0) {
-    const ValueElement* valueElement = findElementWithPrefix (p);
+    valueElement = findElementWithPrefix (p);
     if (valueElement == NULL) {return RC_INVALID_ROMAN_NUMERAL;}
     valueSoFar += valueElement->value;
     p += strlen (valueElement->element);
   }
+  if (valueSoFar > 3999) {return RC_NUMBER_OUT_OF_RANGE;}
   *arabic = valueSoFar;
   return 0;
 }
 
 int convertAtoR (int arabic, char* roman, int len) {
-  return 1;
+  int valueRemaining = arabic;
+  char romanF[80];
+  char* p = romanF;
+  int idx = 0;
+  const ValueElement* valueElement;
+
+  if ((arabic < 1) || (arabic > 3999)) {return RC_NUMBER_OUT_OF_RANGE;}
+  while (valueRemaining > 0) {
+    valueElement = &valueElements[idx];
+    if (valueRemaining >= valueElement->value) {
+      strcpy (p, valueElement->element);
+      p += strlen (valueElement->element);
+      valueRemaining -= valueElement->value;
+    }
+    else {
+      idx++;
+    }
+  }
+  if (strlen (romanF) > len - 1) {return RC_OUTPUT_OVERRUN;}
+  strncpy (roman, romanF, len - 1);
+  return 0;
 }
--- src/converters.h
+++ src/converters.h
@@ -6,5 +6,7 @@ int convertRtoA (const char* roman, int* arabic);
 int convertAtoR (int arabic, char* roman, int len);
 
 int RC_INVALID_ROMAN_NUMERAL;
+int RC_NUMBER_OUT_OF_RANGE;
+int RC_OUTPUT_OVERRUN;
 
 #endif /* CONVERTERS_H_ */
