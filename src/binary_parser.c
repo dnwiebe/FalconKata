3321307dde9460fb1723641dd884c8ef05b93eeb
Dan Wiebe <dwiebe@pillartechnology.com>
2016-08-14 10:50:31 -0400

    No longer uses memory-allocating regular expressions

--- src/binary_parser.c
+++ src/binary_parser.c
@@ -1,12 +1,12 @@
 #include <stddef.h>
+#include <stdio.h>
 #include <string.h>
-#include <regex.h>
 #include "binary_parser.h"
-
 
 const char* nextLine (const char* remainingInput, char* line, int len) {
   const char* in = remainingInput;
   char* out = line;
+
   while ((*in != '\n') && (*in != 0) && ((out - line) < len - 1)) {
     *out++ = *in++;
   }
@@ -14,36 +14,44 @@ const char* nextLine (const char* remainingInput, char* line, int len) {
   return (*in == '\n') ? in + 1 : in;
 }
 
-regex_t LINE_PARSER;
-int LINE_PARSER_COMPILED = 0;
-void initializeLINE_PARSERIfNecessary () {
-  if (LINE_PARSER_COMPILED) {return;}
-  regcomp (&LINE_PARSER, "^\\s*([^ ]+)\\s+([^ ]+)\\s+([^ ]+)\\s*$", REG_EXTENDED);
-  LINE_PARSER_COMPILED = 1;
+int isSpace (char c) {
+  return (c == ' ') || (c == '\t');
 }
 
-char* matchedSubstring (const regmatch_t* match, char* line) {
-  char* result = line + match->rm_so;
-  *(line + match->rm_eo) = 0;
-  return result;
+int findSubstring (char* fromHere, char** begin, char** end, int canEnd) {
+  char *p = fromHere;
+  while (isSpace (*p) && (*p != 0)) {p++;}
+  if (*p == 0) {return 1;}
+  *begin = p;
+  while (!isSpace (*p) && (*p != 0)) {p++;}
+  if ((*p == 0) && !canEnd) {return 1;}
+  *end = p;
+  return 0;
 }
 
 int parseLine (char* line, char** leftOperand, char** operator, char** rightOperand) {
-  regmatch_t matches[4];
-  initializeLINE_PARSERIfNecessary ();
+  char* begins[] = {NULL, NULL, NULL};
+  char* ends[] = {NULL, NULL, NULL};
+  char* fromHere = line;
+  int foundProblem = 0;
+  int i;
 
-  int result = regexec (&LINE_PARSER, line, 4, matches, 0);
-  if (result != 0) {return result;}
+  for (i = 0; i < 3; i++) {
+    foundProblem = findSubstring (fromHere, &begins[i], &ends[i], i == 2);
+    if (foundProblem) {break;}
+    fromHere = ends[i] + 1;
+  }
+  if (foundProblem) {return 1;}
 
-  *leftOperand = matchedSubstring (&matches[1], line);
-  *operator = matchedSubstring (&matches[2], line);
-  *rightOperand = matchedSubstring (&matches[3], line);
-
+  *leftOperand = begins[0]; *ends[0] = 0;
+  *operator = begins[1]; *ends[1] = 0;
+  *rightOperand = begins[2]; *ends[2] = 0;
   return 0;
 }
 
 const BinaryOperation* selectOperation (const BinaryOperation* operations, int count, const char* operator) {
   int i;
+
   for (i = 0; i < count; i++) {
     const BinaryOperation* operation = &operations[i];
     if (!strcmp (operator, operation->operator)) {
@@ -56,6 +64,7 @@ const BinaryOperation* selectOperation (const BinaryOperation* operations, int c
 int strncatWithNewline (char* dest, int len, const char* src) {
   int lenWithTerm = len - 1;
   int resultLeft = lenWithTerm - strlen (dest);
+
   strncat (dest, src, resultLeft);
   resultLeft = lenWithTerm - strlen (dest);
   if (resultLeft <= 0) {
@@ -69,6 +78,43 @@ int strncatWithNewline (char* dest, int len, const char* src) {
   return 0;
 }
 
+void processLine (const BinaryOperation* operations, int count, const char* line, int* returnValue, char* result, int len) {
+  char lineCopy[80];
+  char* leftOperand;
+  char* operator;
+  char* rightOperand;
+  int status;
+  const BinaryOperation* operation;
+  char answer[80];
+
+  do {
+    strncpy (lineCopy, line, sizeof (lineCopy));
+    if (parseLine (lineCopy, &leftOperand, &operator, &rightOperand) != 0) {
+      strncpy (answer, line, sizeof (answer));
+      *returnValue = PBO_INVALID_INPUT;
+      break;
+    }
+
+    operation = selectOperation (operations, count, operator);
+    if (operation == NULL) {
+      strncpy (answer, line, sizeof (answer));
+      *returnValue = PBO_UNKNOWN_OPERATOR;
+      break;
+    }
+
+    if (operation->operation (leftOperand, rightOperand, answer, sizeof (answer)) != 0) {
+      strncpy (answer, line, sizeof (answer));
+      *returnValue = PBO_OPERATOR_FAILED;
+      break;
+    }
+  } while (0);
+
+  status = strncatWithNewline (result, len, answer);
+  if (status != 0) {
+    *returnValue = PBO_OUTPUT_TRUNCATED;
+  }
+}
+
 int PBO_INVALID_INPUT = 1;
 int PBO_UNKNOWN_OPERATOR = 2;
 int PBO_OPERATOR_FAILED = 3;
@@ -77,41 +123,12 @@ int PBO_OUTPUT_TRUNCATED = 4;
 int performBinaryOperations (const BinaryOperation* operations, int count, const char* input, char* result, int len) {
   const char* remainingInput = input;
   char line[80];
-  char lineCopy[80];
-  char* leftOperand;
-  char* operator;
-  char* rightOperand;
-  char answer[80];
-  int status;
   int returnValue = 0;
-  const BinaryOperation* operation;
 
   while ((*remainingInput != 0) && (strlen (result) < len - 1)) {
     remainingInput = nextLine (remainingInput, line, sizeof (line));
 
-    strncpy (lineCopy, line, sizeof (lineCopy));
-    status = parseLine (lineCopy, &leftOperand, &operator, &rightOperand);
-    if (status != 0) {
-      strncpy (answer, line, sizeof (answer));
-      returnValue = PBO_INVALID_INPUT;
-    }
-    else {
-      operation = selectOperation (operations, count, operator);
-      if (operation == NULL) {
-        strncpy (answer, line, sizeof (answer));
-        returnValue = PBO_UNKNOWN_OPERATOR;
-      }
-      else {
-        if (operation->operation (leftOperand, rightOperand, answer, sizeof (answer)) != 0) {
-          strncpy (answer, line, sizeof (answer));
-          returnValue = PBO_OPERATOR_FAILED;
-        }
-      }
-    }
-    status = strncatWithNewline (result, len, answer);
-    if (status != 0) {
-      returnValue = PBO_OUTPUT_TRUNCATED;
-    }
+    processLine (operations, count, line, &returnValue, result, len);
   }
   return returnValue;
 }
