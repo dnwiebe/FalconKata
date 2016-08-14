3e88be835b498d4cd9f416f5b6b1cf5676d87b6a
Dan Wiebe <dwiebe@pillartechnology.com>
2016-08-14 13:05:10 -0400

    Roman arithmetic is hardened a bit

--- src/roman_arithmetic.c
+++ src/roman_arithmetic.c
@@ -1,30 +1,37 @@
 
 #include "roman_arithmetic.h"
 
+int romanBinaryArithmetic (ConversionPair *pair, const char* first, const char* second, char* result, int len, int (*compute) (int, int)) {
+  int firstA;
+  int secondA;
+  int resultA;
+  int status;
+
+  status = pair->romanToArabic (first, &firstA);
+  if (status != 0) {return status;}
+  status = pair->romanToArabic (second, &secondA);
+  if (status != 0) {return status;}
+  resultA = compute (firstA, secondA);
+  status = pair->arabicToRoman (resultA, result, sizeof (result));
+  return status;
+}
+
+int adder (int augend, int addend) {
+  return augend + addend;
+}
+
+int subtracter (int minuend, int subtrahend) {
+  return minuend - subtrahend;
+}
+
 /********************/
 /* PUBLIC INTERFACE */
 /********************/
 
 int addRoman (ConversionPair *pair, const char* augend, const char* addend, char* sum, int len) {
-  int augendA;
-  int addendA;
-  int sumA;
-
-  pair->romanToArabic (augend, &augendA);
-  pair->romanToArabic (addend, &addendA);
-  sumA = augendA + addendA;
-  pair->arabicToRoman (sumA, sum, len);
-  return 0;
+  return romanBinaryArithmetic (pair, augend, addend, sum, len, adder);
 }
 
 int subtractRoman (ConversionPair *pair, const char* minuend, const char* subtrahend, char* sum, int len) {
-  int minuendA;
-  int subtrahendA;
-  int differenceA;
-
-  pair->romanToArabic (minuend, &minuendA);
-  pair->romanToArabic (subtrahend, &subtrahendA);
-  differenceA = minuendA - subtrahendA;
-  pair->arabicToRoman (differenceA, sum, len);
-  return 0;
+  return romanBinaryArithmetic (pair, minuend, subtrahend, sum, len, subtracter);
 }
